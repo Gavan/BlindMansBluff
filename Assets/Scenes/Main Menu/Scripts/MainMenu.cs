f69464b2b538eaeffee8c513fb9a050308bbeb08
gyung <gavan.yung@gmail.com>
2014-01-25 17:24:32 +0800

    centering main menu

--- Assets/Scenes/Main Menu/Scripts/MainMenu.cs
+++ Assets/Scenes/Main Menu/Scripts/MainMenu.cs
@@ -6,15 +6,15 @@ public class MainMenu : MonoBehaviour {
 	
 	void OnGUI () {
 		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		GUI.Box(new Rect(Screen.width/2,10,100,90), "Main Menu");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Start Game")) {
+		if(GUI.Button(new Rect(Screen.width/2+10,40,80,20), "Start Game")) {
 			Application.LoadLevel("Map");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Credits")) {
+		if(GUI.Button(new Rect(Screen.width/2+10,70,80,20), "Credits")) {
 			Application.LoadLevel("Credits");
 		}
 	}
