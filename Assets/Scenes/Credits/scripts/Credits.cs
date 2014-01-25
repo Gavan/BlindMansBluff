b0ecffe819a96438c1f6ae885020c1832d8b5be6
gyung <gavan.yung@gmail.com>
2014-01-25 17:07:06 +0800

    adding credits menu

---
+++ Assets/Scenes/Credits/scripts/Credits.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(0,0,Screen.width,Screen.height), "Credits Screen");
+
+		// Make the back button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,40,80,20), "Back")) {
+			Application.LoadLevel("MainMenu");
+		}
+		
+	}
+}
+
--- Assets/Scenes/Main Menu/Scripts/MainMenu.cs
+++ Assets/Scenes/Main Menu/Scripts/MainMenu.cs
@@ -9,13 +9,13 @@ public class MainMenu : MonoBehaviour {
 		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
-			Application.LoadLevel(1);
+		if(GUI.Button(new Rect(20,40,80,20), "Start Game")) {
+			Application.LoadLevel("Map");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
-			Application.LoadLevel(2);
+		if(GUI.Button(new Rect(20,70,80,20), "Credits")) {
+			Application.LoadLevel("Credits");
 		}
 	}
 }
