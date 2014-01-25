9be80ed338a780cd3fce95cfcc5bc92a0c4cab68
Ken Syuu <kensyuu@hotmail.com>
2014-01-25 18:20:07 +0800

    alpha v1 , Ray

---
+++ Assets/Scripts/my3rdController.js
@@ -0,0 +1,99 @@
+
+// Require a character controller to be attached to the same game object
+@script RequireComponent(CharacterController)
+
+public var walkMaxAnimationSpeed : float = 0.75;
+public var trotMaxAnimationSpeed : float = 1.0;
+public var runMaxAnimationSpeed : float = 1.0;
+public var jumpAnimationSpeed : float = 1.15;
+public var landAnimationSpeed : float = 1.0;
+
+private var _animation : Animation;
+
+private var _characterState : CharacterState;
+
+// The speed when walking
+var walkSpeed = 2.0;
+// after trotAfterSeconds of walking we trot with trotSpeed
+var trotSpeed = 4.0;
+// when pressing "Fire3" button (cmd) we start running
+var runSpeed = 6.0;
+
+var inAirControlAcceleration = 3.0;
+
+// How high do we jump when pressing jump and letting go immediately
+var jumpHeight = 0.5;
+
+// The gravity for the character
+var gravity = 20.0;
+// The gravity in controlled descent mode
+var speedSmoothing = 10.0;
+var rotateSpeed = 500.0;
+var trotAfterSeconds = 3.0;
+
+var canJump = true;
+
+private var jumpRepeatTime = 0.05;
+private var jumpTimeout = 0.15;
+private var groundedTimeout = 0.25;
+
+// The camera doesnt start following the target immediately but waits for a split second to avoid too much waving around.
+private var lockCameraTimer = 0.0;
+
+// The current move direction in x-z
+private var moveDirection = Vector3.zero;
+// The current vertical speed
+private var verticalSpeed = 0.0;
+// The current x-z move speed
+private var moveSpeed = 0.0;
+
+// The last collision flags returned from controller.Move
+private var collisionFlags : CollisionFlags; 
+
+// Are we jumping? (Initiated with jump button and not grounded yet)
+private var jumping = false;
+private var jumpingReachedApex = false;
+
+// Are we moving backwards (This locks the camera to not do a 180 degree spin)
+private var movingBack = false;
+// Is the user pressing any keys?
+private var isMoving = false;
+// When did the user start walking (Used for going into trot after a while)
+private var walkTimeStart = 0.0;
+// Last time the jump button was clicked down
+private var lastJumpButtonTime = -10.0;
+// Last time we performed a jump
+private var lastJumpTime = -1.0;
+
+
+// the height we jumped from (Used to determine for how long to apply extra jump power after jumping.)
+private var lastJumpStartHeight = 0.0;
+
+
+private var inAirVelocity = Vector3.zero;
+
+private var lastGroundedTime = 0.0;
+
+
+private var isControllable = true;
+
+function Update() {
+	
+	if (!isControllable)
+	{
+		// kill all inputs if not controllable.
+		Input.ResetInputAxes();
+	}
+
+	if (Input.GetButtonDown ("Jump"))
+	{
+		lastJumpButtonTime = Time.time;
+	}
+
+	var v = Input.GetAxisRaw("Vertical") *  0.15;
+	var h = Input.GetAxisRaw("Horizontal") * 0.15;
+
+	transform.Translate(h, 0, v);
+			
+}
+
